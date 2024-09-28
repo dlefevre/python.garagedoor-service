2e8fa40a98ee326b5ad8991c6f8ede8d30a2bc40
Dusty Lefevre <dusty.lefevre@vlaanderen.be>
2024-09-28 19:58:19 +0000

    Implement GPIO Mock and Pi adapters. Add two basic tests for Rest Client in vscode. Add config management. Add authentication with api keys. Built daemon thread for manageing toggle events.

---
+++ garagedoor_service/main.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from fastapi import FastAPI, HTTPException, Security
+from fastapi.security import APIKeyHeader
+from typing import Any
+import garagedoor_service.door_controller as door_controller
+import garagedoor_service.tools.api_key as api_key_tools
+
+app = FastAPI()
+
+api_key_header = APIKeyHeader(name="x-api-key", auto_error=True)
+
+
+def verify_api_key(api_key: str = Security(api_key_header)) -> str:
+    """Verify the API key.
+    
+    Parameters:
+        api_key (str): The API key.
+        
+    Returns:
+        str: The API key.
+        
+    Raises:
+        HTTPException: If the API key is invalid.
+    """
+    if not api_key_tools.verify(api_key):
+        raise HTTPException(status_code=403, detail="Unauthorized")
+    return api_key
+
+
+@app.post("/toggle")
+def toggle_door(api_key: str = Security(verify_api_key)) -> Any:
+    try:
+        door_controller.request_door_toggle()
+    except Exception as e:
+        return {"result": "nok", "message": str(e)}
+    return {"result": "ok"}
+
+
+@app.get("/state")
+def get_status(api_key: str = Security(verify_api_key)) -> Any:
+    try:
+        state = door_controller.get_door_state()
+    except Exception as e:
+        return {"result": "nok", "message": str(e)}
+    return {"result": "ok", "state": state}
+
+
+@app.get("/readyz")
+def readyz() -> Any:
+    return {"result": "ok"}
+
+
+@app.get("/healthz")
+def healthz() -> Any:
+    return {"result": "ok"}
